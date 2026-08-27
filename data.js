0fd7c8efbcd17172e060ac61ae454070b5731f34
Chris Tomsett <90329606+christomsett@users.noreply.github.com>
2026-08-27 11:20:22 +0100

    Update data.js

--- data.js
+++ data.js
@@ -176,8 +176,9 @@ var APP_DATA = {
       "infoHotspots": [],
       "cameraHotspots": [
         {
-          "yaw": 0.7,
-          "pitch": -0.8,
+          "yaw": 0.57,
+          "pitch": 0.04,
+          "img": "img/Heritage.png"
           "category": "heritage",
           "title": "The Needles Visitor Attraction",
           "text": "The centre boasts a number of attractions for visitors, including chairlifts to the beach, a sand shop, and rides.",
